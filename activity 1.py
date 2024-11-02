9683f8c37920a35a06991e6eb50aa283340059e9
siddharth0212-hash <sidsuperkid02@gmail.com>
2024-11-02 14:47:52 +0300

    activty - 1

---
+++ activity 1.py
@@ -0,0 +1,13 @@
+# Program to show Constant time Complexity
+
+def printnumber(n):
+    iteration=0
+    print("The number entered by the user is ",n)
+    iteration+=1
+    print("Total Iterations done by the code is ", iteration, "\n")
+
+
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time take by our code wont change")
